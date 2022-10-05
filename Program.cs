a94c534097be56160cff67189574f0c7f99ebee0
de3mi88 <vladimirturbabin@gmail.com>
2022-10-05 10:42:08 +0300

    make function GetSecondarray

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
 ﻿string[] arrayFirst = { "*-*", "Hello Rustem", "-_-", "20", "superhero", "-1" };
+
+string[] GetSecondarray(string[] array)
+{
+   string[] array2 = { "", "", "", "","","" };
+    int m=3;
+    for (int i = 0; i < array.Count(); i++)
+    {
+                if (array[i].Length <= m)
+                {
+                    array2[i] = array[i];
+                }
+    }
+    return array2;
+}
